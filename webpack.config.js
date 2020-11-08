38ac6f12d80ee1336f555d9e6af70f776a225c23
D3SOX <d3sox@protonmail.com>
2020-11-08 01:40:10 +0100

    chore: prevent caching via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
+        filename: '[name].[contenthash].js',
     },
     module: {
         rules: [
