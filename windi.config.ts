081a48364f5617919a9bb672321075438debe0e1
D3SOX <d3sox@protonmail.com>
2021-09-07 22:29:54 +0200

    fix: add new row breakpoints to fix wrapping on wide screens

---
+++ windi.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+    theme: {
+        flex: {
+            1: '1 1 0%',
+            auto: '1 1 auto',
+            initial: '0 1 auto',
+            fill: '1 0 100%',
+            inherit: 'inherit',
+            none: 'none',
+            2: '2 2 0%',
+        },
+    }
+})
