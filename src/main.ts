bdda3597a9b318de78b2cb42dc38433a3f645ded
D3SOX <nico@d3sox.me>
2026-04-22 21:19:07 +0200

    fix: replace discord with matrix

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,3 @@ import 'jetbrains-mono/css/jetbrains-mono.css';
 
 import Iconify from '@iconify/iconify';
 Iconify.scan();
-
-const tooltips = document.querySelectorAll('.tooltip');
-
-for (const tooltip of Array.from(tooltips)) {
-    const text = tooltip.firstElementChild!.innerHTML;
-    tooltip.addEventListener('click', async () => {
-        await navigator.clipboard.writeText(text);
-    });
-}
